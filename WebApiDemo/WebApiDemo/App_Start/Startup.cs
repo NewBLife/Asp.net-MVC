b5c1164f86f99ac4a5b4d2161e1b54bd2db3ee77
李暁賓 <newblifes@hotmail.com>
2016-05-22 17:21:17 +0900

    OAUTH WebAPi Demo

---
+++ WebApiDemo/WebApiDemo/App_Start/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Web.Http;
+using WebApiDemo.App_Start;
+using WebApiDemo.Providers;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace WebApiDemo.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            ConfigreOAuth(app);
+            WebApiConfig.Register(config);
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+        public void ConfigreOAuth(IAppBuilder app)
+        {
+            var oauthServerOptions = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(oauthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
---
+++ WebApiDemo/WebApiDemo/Providers/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace WebApiDemo.Providers
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            if (context.UserName == null)
+            {
+                context.SetError("invalid_grant", "用户名或者密码不正确");
+                return;
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+        }
+
+        public override Task GrantRefreshToken(OAuthGrantRefreshTokenContext context)
+        {
+            return base.GrantRefreshToken(context);
+        }
+    }
+}
